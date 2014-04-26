c5a9601731bcc56456111515ffda51c26f6095d8
sacry- <nitsche25@gmail.com>
2014-04-26 14:14:06 +0200

    account now has several emails

--- aufgabe_2/src/DataTypePackage/Account.java
+++ aufgabe_2/src/DataTypePackage/Account.java
@@ -1,32 +1,54 @@
 package DataTypePackage;
 
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by Allquantor on 20.04.14.
  */
 public class Account {
 
+    private Integer uid;
     private String pw;
-    private String email;
+    private Set<String> eAddress;
     private String storageDir;
 
-    private Account(String pw, String email, String storageDir) {
+    private Account(String pw, Set<String> eAddress, String storageDir, Integer uid) {
         this.pw = pw;
-        this.email = email;
+        this.eAddress = eAddress;
+        this.storageDir = storageDir;
+        this.uid = uid;
     }
 
-    public static Account valueOf(String pw, String email, String storageDir) {
-        return new Account(pw, email, storageDir);
+    public static Account valueOf(String pw, Set<String> email, String storageDir, Integer uid) {
+        return new Account(pw, email, storageDir, uid);
     }
 
     public String password() {
         return pw;
     }
 
-    public String email() {
-        return email;
+    public Set<String> eAddress() {
+        return eAddress;
     }
 
-    public String storageDir(){
+    public String storageDir() {
         return storageDir;
+    }
+
+    public Integer uid() {
+        return uid;
+    }
+
+    public boolean add(String address) {
+        return eAddress.add(address);
+    }
+
+    public boolean addAll(Set<String> address) {
+        return eAddress.addAll(address);
+    }
+
+    public boolean delete(String address) {
+        return eAddress.remove(address);
     }
 }
