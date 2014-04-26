62df658a87c835e645334c13aab33616da17fdbd
sacry- <nitsche25@gmail.com>
2014-04-26 14:03:30 +0200

    storage dir

--- aufgabe_2/src/DataTypePackage/Account.java
+++ aufgabe_2/src/DataTypePackage/Account.java
@@ -7,18 +7,26 @@ public class Account {
 
     private String pw;
     private String email;
+    private String storageDir;
 
-    private Account(String pw, String email) {
+    private Account(String pw, String email, String storageDir) {
         this.pw = pw;
         this.email = email;
     }
 
-    public static Account valueOf(String pw, String email) {
-        return new Account(pw, email);
+    public static Account valueOf(String pw, String email, String storageDir) {
+        return new Account(pw, email, storageDir);
     }
-    
-    public String password() { return pw; }
-    public String email() { return email; }
 
+    public String password() {
+        return pw;
+    }
 
+    public String email() {
+        return email;
+    }
+
+    public String storageDir(){
+        return storageDir;
+    }
 }
