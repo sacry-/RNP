79ac7655e772ffa5a9b6ea8d6c944a33b44f681a
GollyTicker <golly.ticker@gmail.com>
2014-04-26 13:40:45 +0200

    dummy JUnit Test and Demo Run file

---
+++ aufgabe_2/src/Tests/Run.java
@@ -0,0 +1,12 @@
+package Tests;
+
+public class Run {
+	
+	public static void main(String[] args) {
+		System.out.println("Connect to Server...");
+		System.out.println("Send Emails...");
+		System.out.println("Get Emails...");
+		System.out.println("Disconnect from Server...");		
+	}
+	
+}
