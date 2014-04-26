fd27c6e3e6e36be65e020c61b97c8b1dd69a5d7a
GollyTicker <golly.ticker@gmail.com>
2014-04-26 13:40:10 +0200

    dummy methods saving and loading

--- aufgabe_2/src/ServicePackage/StorageService.java
+++ aufgabe_2/src/ServicePackage/StorageService.java
@@ -1,5 +1,8 @@
 package ServicePackage;
 
+import DataTypePackage.Account;
+import DataTypePackage.Email;
+
 /**
  * Created by Allquantor on 20.04.14.
  */
@@ -9,8 +12,21 @@ package ServicePackage;
     //PATHS
     public static final String EMAIL_STORE_PATH = "";
     public static final String ACCOUNTDATA_STORE_PATH = "";
+    
+    public static boolean save(Email a) {
+    	return false;
+    }
 
-
-
-
+    public static boolean save(Account a) {
+    	return false;
+    }
+    
+    public static Email loadEmail() {
+    	return null;
+    }
+    
+    public static Account loadAccount() {
+    	return null;
+    }
+    
 }
