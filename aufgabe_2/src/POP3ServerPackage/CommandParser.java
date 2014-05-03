7d642aa23c78fc544467bfbd57e47f44ace671e4
GollyTicker <golly.ticker@gmail.com>
2014-05-03 14:34:16 +0200

    bits comment and fixed warnings

--- aufgabe_2/src/POP3ServerPackage/CommandParser.java
+++ aufgabe_2/src/POP3ServerPackage/CommandParser.java
@@ -35,13 +35,17 @@ public class CommandParser {
             //if not int error
             return transaction.retr(1);
         } else if (cmd == DELE) {
-            return transaction.dele(arg);
+        	// parse int
+        	// if not int then error.
+            return transaction.dele(1);
         } else if (cmd == NOOP) {
             return transaction.noop(arg);
         } else if (cmd == RSET) {
             return transaction.rset();
         } else if (cmd == UIDL) {
-            return transaction.uidl(arg);
+            //check arg given or not
+            //if given parse int
+            return transaction.uidl(1);
         } else {
             return ServerCodes.fail("Unknown Command " + cmd);
         }
