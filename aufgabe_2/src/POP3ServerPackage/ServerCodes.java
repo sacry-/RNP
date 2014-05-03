eeb25566a4666a4fa1545ccfefbeab180d1d2e31
GollyTicker <golly.ticker@gmail.com>
2014-05-03 14:50:59 +0200

    added failWithMsgNo

--- aufgabe_2/src/POP3ServerPackage/ServerCodes.java
+++ aufgabe_2/src/POP3ServerPackage/ServerCodes.java
@@ -5,31 +5,35 @@ package POP3ServerPackage;
  */
 public class ServerCodes {
 
-    public static final String OK = "+OK";
-    public static final String ERROR = "-ERR";
+	public static final String OK = "+OK";
+	public static final String ERROR = "-ERR";
 
-    public static final String USER = "USER";
-    public static final String PASS = "PASS";
-    public static final String QUIT = "QUIT";
-    public static final String STAT = "STAT";
-    public static final String LIST = "LIST";
-    public static final String RETR = "RETR";
-    public static final String DELE = "DELE";
-    public static final String NOOP = "NOOP";
-    public static final String RSET = "RSET";
-    public static final String UIDL = "UIDL";
-    public static final String NEWLINE = System.getProperty("line.separator");
-    public static final String MULTI_LINE_TERMINATOR = NEWLINE + ".";
+	public static final String USER = "USER";
+	public static final String PASS = "PASS";
+	public static final String QUIT = "QUIT";
+	public static final String STAT = "STAT";
+	public static final String LIST = "LIST";
+	public static final String RETR = "RETR";
+	public static final String DELE = "DELE";
+	public static final String NOOP = "NOOP";
+	public static final String RSET = "RSET";
+	public static final String UIDL = "UIDL";
+	public static final String NEWLINE = System.getProperty("line.separator");
+	public static final String MULTI_LINE_TERMINATOR = NEWLINE + ".";
 
-    private ServerCodes(){
+	private ServerCodes() {
 
-    }
+	}
 
-    public static String success(String message) {
-        return (OK + " " + message);
-    }
+	public static String success(String message) {
+		return (OK + " " + message);
+	}
 
-    public static String fail(String message) {
-        return (ERROR + " " + message);
-    }
+	public static String fail(String message) {
+		return (ERROR + " " + message);
+	}
+
+	public static String failWithInvalidMsgNo(String arg) {
+		return fail("Invalid Msg No " + arg);
+	}
 }
